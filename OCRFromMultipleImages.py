fd21b9cb12830c3afea17493dfbcc86c8d2b49d8
karanrawat2q <karanrawat2q@gmail.com>
2021-08-05 16:57:34 +0530

    via commit

---
+++ OCRFromMultipleImages.py
@@ -0,0 +1,14 @@
+import os
+import glob
+
+path = r"C:\Users\Karan\Pictures\Screenshots\*.png"
+pics = []
+for img in glob.glob(path):
+    pics.append(img)
+
+os.chdir("C:\\Users\\Karan\\Pictures\\Screenshots")
+
+for fileidx, filename in enumerate(pics):
+    filename_clean = filename.replace("\\", "/")
+    outfile = f"file{fileidx}"
+    os.system(f"tesseract {filename_clean} {outfile}")
